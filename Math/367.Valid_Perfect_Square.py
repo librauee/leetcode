58cc4ab4aa244c54e8f14491c665736d9203360b
librauee <870407139@qq.com>
2019-04-28 13:55:48 +0800

    判平方数

---
+++ Math/367.Valid_Perfect_Square.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 10:27:13 2019
+
+@author: Administrator
+"""
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        return int(num ** 0.5)**2 == num
