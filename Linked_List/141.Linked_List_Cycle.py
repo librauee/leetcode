c545ad94bbe2b0f1e060609aa99efc41726fd935
librauee <870407139@qq.com>
2019-06-23 21:49:47 +0800

    判断列表是否有环

---
+++ Linked_List/141.Linked_List_Cycle.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 08:13:28 2019
+
+@author: Administrator
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        p1=p2=head
+        while p2.next and p2.next.next:
+            p1=p1.next
+            p2=p2.next.next
+            if p1==p2:
+                return True
+        return False
